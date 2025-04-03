9424181dd96541582b19c20d93f5ca223d558db8
Saul-P3R <straydogs567@gmail.com>
2025-04-03 12:05:36 -0600

    Add files via upload

--- Holamundo/handposedibujo/sketch.js
+++ Holamundo/handposedibujo/sketch.js
@@ -2,25 +2,33 @@ let handPose;
 let video;
 let hands = [];
 
-
 function preload() {
-  handPose = ml5.handPose();
+  handPose = ml5.handPose({flipped:true});
 }
+
+function mousePressed(){
+  console.log(hands);
+}
+
 function setup() {
   createCanvas(640, 480);
-  // Create the video and hide it
-  video = createCapture(VIDEO);
-  video.size(640, 480);
-  video.hide();
-  // Start detecting hands from the webcam video
-  handPose.detectStart(video, gotHands);
-
+  painting = createGraphics (640,480);
+  painting.clear ();
+    // Create the video and hide it
+    video = createCapture(VIDEO,{flipped: true});
+    video.size(640, 480);
+    video.hide();
+  
+   // Start detecting hands from the webcam video
+   handPose.detectStart(video, gotHands);
 }
+
 // Callback function for when handPose outputs data
 function gotHands(results) {
   // Save the output to the hands variable
   hands = results;
 }
+
 function draw() {
   image(video, 0, 0, width, height);
   // Draw all the tracked hand points
@@ -28,11 +36,15 @@ function draw() {
     let hand = hands[i];
     for (let j = 0; j < hand.keypoints.length; j++) {
       let keypoint = hand.keypoints[j];
-      fill(0, 255, 0);
-      noStroke();
-      circle(keypoint.x, keypoint.y, 10);
+      let index = hand.keypoints [0];
+      //fill(0, 255, 0);
+      painting.noStroke();
+      //circle(keypoint.x, keypoint.y, 10);
+      painting.fill(0,0,255);
+      painting.circle (index.x, index.y, 5, 5);
     }
   }
+  image(painting, 0, 0);
 }
 
 
